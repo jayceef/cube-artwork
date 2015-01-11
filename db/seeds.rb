45a160e8de6316143a382e436af1a9ff8c66f290
Maxwell Mears <maxwellmears@gmail.com>
2015-01-10 17:56:52 -0800

    Add infrastructure for comments

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,9 +3,18 @@ require 'image_suckr'
 suckr = ImageSuckr::GoogleSuckr.new
 
 10.times do
-  Post.create(
+  post = Post.create(
     photo_url: suckr.get_image_url,
     title: Faker::Lorem.sentence,
     body: Faker::Lorem.paragraph
   )
+
+  (1..10).to_a.sample.times do
+    post.comments.create(
+      username: Faker::Internet.user_name,
+      body: Faker::Lorem.paragraph
+    )
+  end
 end
+
+
